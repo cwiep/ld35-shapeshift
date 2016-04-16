2cb31b147825d05dcbcfd8cff837b85a8766289c
Christian Wieprecht <christian.wieprecht@udo.edu>
2016-04-16 19:48:30 +0200

    Removed horrible bugged AnimationPlayer. Killing the prof.

--- player.gd
+++ player.gd
@@ -2,7 +2,7 @@
 extends Area2D
 
 var player_image
-var anim_node
+var animation_timer
 
 # variables for moving up/down stairs
 var can_use_stairs
@@ -13,51 +13,69 @@ var current_stairs
 # collecting targets
 var can_collect_target
 
+# shapeshifting
+var is_attacking
+
 func _ready():
+	set_process_input(true)
 	set_fixed_process(true)
 	player_image = get_node("Sprite")
-	anim_node = get_node("Sprite/walk_anim")
 	can_use_stairs = false
 	is_using_stairs = false
 	can_collect_target = false
+	is_attacking = false
+	animation_timer = 0
 	
+func _input(event):
+	if event.is_action_pressed("ui_accept") and !event.is_echo() and !is_attacking:
+		if get_node("person_look_left").is_colliding():
+			if get_node("person_look_left").get_collider().is_in_group("prof"):
+				is_attacking = true
+				get_node("person_look_left").get_collider().kill()
+			
 func _fixed_process(delta):
 	# check for collectible objectives
 	if can_collect_target:
 		get_node("../hud/target").set_text("collect")
 	else:
 		get_node("../hud/target").set_text("")
-		
-	if Input.is_action_pressed("ui_accept"):
-		get_node("../guests/professor").kill()
-		
+	
+	# update animation
+	animation_timer += delta
+	if animation_timer >= 0.8:
+		animation_timer = 0
+		# any attack ends after one animation cycle
+		is_attacking = false
+	
 	if is_using_stairs:
-		# move player toward target
+		# move player towards stairs target
 		set_pos(Vector2(lerp(get_pos().x, stairs_move_target.x, 0.05), lerp(get_pos().y, stairs_move_target.y, 0.05)))
 		if get_pos().distance_to(stairs_move_target) < 3:
 			set_pos(stairs_move_target)
 			is_using_stairs = false
 			stairs_move_target = current_stairs.get_stairs_target(get_pos())
+		# todo: play normal walk animation
+	elif is_attacking:
+		print("is attacking")
 	else:
 		# otherwise allow normal motion
 		if Input.is_action_pressed("ui_left") and !get_node("look_left").is_colliding():
 			translate(Vector2(-400 * delta, 0))
-				
-			if anim_node.get_current_animation() != "walk_left":
-				anim_node.set_current_animation("walk_left")
+			
+			# animate player walking
+			player_image.set_flip_h(false)
+			player_image.set_frame(int(animation_timer / 0.2))
 
 		elif Input.is_action_pressed("ui_right") and !get_node("look_right").is_colliding():
 			translate(Vector2(400 * delta, 0))
 
-			if anim_node.get_current_animation() != "walk_right":
-				anim_node.set_current_animation("walk_right")
+			# animate player walking
+			player_image.set_flip_h(true)
+			player_image.set_frame(int(animation_timer / 0.2))
 
 		else:
-			if player_image.is_flipped_h():
-				anim_node.set_current_animation("stand_right")
-			else:
-				anim_node.set_current_animation("stand_left")
-		anim_node.advance(delta)
+			player_image.set_frame(0)
+		
 		if Input.is_action_pressed("ui_accept") and can_use_stairs:
 			is_using_stairs = true
 		
