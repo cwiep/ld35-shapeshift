b5aaf63f0fdb8ccca446e76e1b0b55de17207be3
Christian Wieprecht <christian.wieprecht@udo.edu>
2016-04-17 19:52:33 +0200

    Cool animation for shapeshift.

--- player.gd
+++ player.gd
@@ -20,6 +20,9 @@ var is_attacking
 const ray_left = Vector2(-30, 0)
 const ray_right = Vector2(30, 0)
 var current_shape
+const attack_frames = [11, 8, 9, 10, 10, 9, 8, 11]
+var attack_timer
+var changed_shape
 
 var global_node
 
@@ -35,6 +38,8 @@ func _ready():
 	is_attacking = false
 	animation_timer = 0
 	global_node = get_node("/root/global")
+	attack_timer = 0
+	changed_shape = false
 	
 func _input(event):
 	if is_attacking:
@@ -48,37 +53,24 @@ func _input(event):
 			var collider = colliding_ray.get_collider()
 			if collider.is_in_group("prof"):
 				is_attacking = true
-				collider.kill()
-				get_node("SamplePlayer").play("hit")
 				# change player sprite to killed type
-				var flip = player_image.is_flipped_h()
-				player_image.hide()
-				player_image = get_node("sprite_prof_m")
-				player_image.show()
-				player_image.set_flip_h(flip)
 				current_shape = "prof_m"
 			elif collider.is_in_group("guest_w"):
 				is_attacking = true
-				collider.kill()
 				get_node("SamplePlayer").play("hit")
 				# change player sprite to killed type
-				var flip = player_image.is_flipped_h()
-				player_image.hide()
-				player_image = get_node("sprite_guest_w")
-				player_image.show()
-				player_image.set_flip_h(flip)
 				current_shape = "guest_w"
 			elif collider.is_in_group("guest_m"):
 				is_attacking = true
+				# change player sprite to killed type
+				current_shape = "guest_m"
+			
+			if is_attacking:
+				attack_timer = 0
+				changed_shape = false
 				collider.kill()
 				get_node("SamplePlayer").play("hit")
-				# change player sprite to killed type
-				var flip = player_image.is_flipped_h()
-				player_image.hide()
-				player_image = get_node("sprite_guest_m")
-				player_image.show()
-				player_image.set_flip_h(flip)
-				current_shape = "guest_m"
+			
 	elif event.is_action_pressed("ui_collect") and !event.is_echo():
 		pass
 		
@@ -87,9 +79,12 @@ func _fixed_process(delta):
 	animation_timer += delta
 	if animation_timer >= 0.8:
 		animation_timer = 0
-		# any attack ends after one animation cycle
-		is_attacking = false
-	
+
+	if is_attacking:
+		attack_timer += delta
+		if attack_timer >= 1.6:
+			is_attacking = false
+			
 	if is_using_stairs:
 		# move player towards stairs target
 		set_pos(Vector2(lerp(get_pos().x, stairs_move_target.x, 0.05), lerp(get_pos().y, stairs_move_target.y, 0.05)))
@@ -99,8 +94,12 @@ func _fixed_process(delta):
 			stairs_move_target = current_stairs.get_stairs_target(get_pos())
 		player_image.set_frame(int(animation_timer / 0.2))
 	elif is_attacking:
-		# attack is a still frame that lasts one animation cycle and blocks all movement
-		player_image.set_frame(11)
+		# attack lasts two animation cycles and blocks all movement
+		# player_image.set_frame(11)
+		if attack_timer > 0.8 and !changed_shape:
+			changed_shape = true
+			_set_sprite_for_type(current_shape)
+		player_image.set_frame(attack_frames[int(attack_timer / 0.2)])
 	else:
 		# otherwise allow normal motion
 		if Input.is_action_pressed("ui_left"):
@@ -173,3 +172,18 @@ func _on_player_area_exit( area ):
 			
 func causes_panic():
 	return is_attacking
+	
+func _set_sprite_for_type(type):
+	# hide current shape
+	player_image.hide()
+	var flip = player_image.is_flipped_h()
+	# set new shape
+	if type == "prof_m":
+		player_image = get_node("sprite_prof_m")
+	if type == "guest_w":
+		player_image = get_node("sprite_guest_w")
+	if type == "guest_m":
+		player_image = get_node("sprite_guest_m")
+	player_image.show()
+	# set correct direction of new shape
+	player_image.set_flip_h(flip)
