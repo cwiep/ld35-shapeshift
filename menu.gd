c4262294a983c86bff17cb98390dc5745b388539
Christian Wieprecht <christian.wieprecht@udo.edu>
2016-04-17 20:22:14 +0200

    Added menu and manual.

--- menu.gd
+++ menu.gd
@@ -9,7 +9,12 @@ func _ready():
 	set_process_input(true)
 	
 func _input(event):
-	if event.is_action_pressed("ui_accept") and !event.is_echo():
-		get_tree().change_scene("res://house.tscn")
+	if event.is_action_released("ui_right") and !event.is_echo():
+		if get_node("Sprite").get_frame() == 0:
+			get_node("Sprite").set_frame(1)
+		else:
+			get_tree().change_scene("res://house.tscn")
+	elif event.is_action_released("ui_left") and !event.is_echo():
+		get_node("Sprite").set_frame(0)
 
 
